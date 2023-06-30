5b1bd3eefb5d2e05d1cdc4b3f1b13c49ffaab231
jiho <jihopark09@naver.com>
2023-07-01 00:13:37 +0900

    API fetch using Future Builder

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 ## Commits
 
-API fetch using State(6/30) : Fetched data from API using 'State'
+API fetch using State(6/30) : Fetched data from API using 'State' (StatefulWidget)
+
+API fetch using Future builder(7/1) : Fetched data from API using Future builder (StatelessWidget)
--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -2,34 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  // HTTP 요청을 기다리는 함수
-  void waitForWebtoons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebtoons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -42,6 +21,18 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         elevation: 2, // 그림자
+      ),
+      // FutureBuilder : StateLess 에서도 가능 **********
+      body: FutureBuilder(
+        future: webtoons, // await 안써도됨(FutureBuilder 가 붙여줌)
+        builder: (context, snapshot) {
+          // snapshot : Future의 상태를 알 수 있음
+          if (snapshot.hasData) {
+            // Future가 완료돼서 데이터가 존재하면
+            return const Text("There is data!");
+          }
+          return const Text("Loading...");
+        },
       ),
     );
   }
